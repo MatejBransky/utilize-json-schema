4ca5ae23a8645d02afce7558f1dd8238e91f5db3
Matej Bransky <matej.bransky@ataccama.com>
2025-03-21 13:30:41 +0100

    fix builds and deps

--- example/index.ts
+++ example/index.ts
@@ -1,12 +1,12 @@
-import { compile } from "@utilize/json-schema-to-typescript";
+import { compile } from '@utilize/json-schema-to-typescript';
 
-import { readFileSync, writeFileSync } from "node:fs";
+import { readFileSync, writeFileSync } from 'node:fs';
 
 async function generate() {
-  writeFileSync(
-    "person.d.ts",
-    await compile(JSON.parse(readFileSync("person.json", "utf-8")), "Person"),
-  );
+	writeFileSync(
+		'person.d.ts',
+		await compile(JSON.parse(readFileSync('person.json', 'utf-8')), 'Person')
+	);
 }
 
 generate();
